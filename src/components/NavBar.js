f7388f9ef346ab21817f10c2396697fdd0f57de1
Julie Poulain <juliempoulain@gmail.com>
2024-04-05 14:52:04 -0700

    add props etc

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((linkName) => {
+    return <a key={linkName} href={"#"+linkName}>{linkName}</a>
+  })
+  return <nav>
+    {linkElements}
+    </nav>;
 }
 
 export default NavBar;
